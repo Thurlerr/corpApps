ca396d223cd0fb9032843e459cfd920387542f4b
Thurlerr <andrew_thurler@hotmail.com>
2025-05-31 23:33:52 -0300

    terminar classe emprestimo

--- src/main/java/com/model/Emprestimo.java
+++ src/main/java/com/model/Emprestimo.java
@@ -10,10 +10,28 @@ public class Emprestimo {
         this.cliente = cliente;
         this.valorDesejado = valorDesejado;
         this.parcelas = parcelas;
-        //retornar o valor?
     }
 
-    //colocar a lógica de calculo de simulação de empréstimo aqui
+    public void simularEmprestimo ( double txJurosAnual){
+        //essa lógica de calculo pode estar dentro das regras de negócio do banco, e não precisa ser repetida aqui na parte de simulação?
+        //essa taxa de juros anual tem que vir da escolha do banco
+        double capacidadeDePagamento = cliente.getRenda() * 0.30;
 
-    cliente.adicionarEmprestimo(this); //usar o factory method do gpt pois ele ainda não está calculado
+        if (capacidadeDePagamento < this.valorDesejado){ //O empréstimo só é liberado para clientes que comprometam até 30% da renda.
+            System.out.println("Valor desejado excede capacidade de pagamento com renda atual.");
+        }
+        //Valor total (CET) = valor desejado * (1 + (tx de juros mensal * parcelas))
+        double taxaJurosMensal = (txJurosAnual/100)/12;
+        double CET = this.valorDesejado * (1 + (taxaJurosMensal * this.parcelas));
+        double valorParcelas = CET/this.parcelas;
+
+        //exibir resultado
+        System.out.println("Taxa de juros mensal:" + taxaJurosMensal*100);
+        System.out.println("Custo Efetivo Total (CET): " + CET);
+        System.out.println(this.parcelas + "parcelas de R$: " + valorParcelas);
+
+
+    }
+
+    cliente.adicionarEmprestimo(this); //usar o factory method do gpt ou achar outra forma de adicionar esse emprestimo a lista de clientes
 }
